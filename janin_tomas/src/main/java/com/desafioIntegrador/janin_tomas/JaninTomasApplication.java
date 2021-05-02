55eb12accdff988aa4df3fa5c9aaad18074def05
totojanin <tomas.janin@mercadolibre.com>
2021-05-02 16:20:01 -0300

    Create Spring Project

---
+++ janin_tomas/src/main/java/com/desafioIntegrador/janin_tomas/JaninTomasApplication.java
@@ -0,0 +1,13 @@
+package com.desafioIntegrador.janin_tomas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaninTomasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaninTomasApplication.class, args);
+	}
+
+}
